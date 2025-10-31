8c6d1726da6866337cbd9ca9c2506f2d0f5ef4ed
mrthiitvn123 <mrthiitvn@gmail.com>
2025-10-31 10:01:45 +0700

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,6 @@
+
+
+// ===== Global mutable state & hardcoded secrets (Security, Config) =====
+var g_TOKEN = "Bearer hardcoded-super-secret-token";
+let ccc = null as any;
+let abc = null;  // ambiguous name
